64e0e469b414dc68c13c8565bd4e72f70a54096c
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-01-25 23:54:58 +0900

    make mod func

---
+++ chap1/pra1-4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int mod(int a, int b, int c)
+{
+	if (a >= b)
+	{
+		if (b >= c)
+			return b;
+		else if (a <= c)
+			return a;
+		else
+			return c;
+	} else if (a > c)
+	{
+		return a;
+	} else if (b > c)
+	{
+		return c;
+	} else
+	{
+		return b;
+	}
+}
+
+
+int main(void)
+{
+	return 0;
+}
